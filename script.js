3ad1b364a64c429ff3362cdcf00029e441d3bc93
VAIBHAV SHARMA <66861535+vaibhav311@users.noreply.github.com>
2022-01-19 02:39:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// // var audio = document.createElement('audio');
+// // audio.setAttribute('src','sound.mp3');
+// // audio.setAttribute('autoplay','true');
+// // audio.loop=true;
+// var audio = new Audio('sound.mp3');
+// audio.play();
+// // audio.play();
+
+// // console.log('hello');
+
+var audio =  document.getElementById('Audio'); 
+audio.play();
+audio.muted = false;
+// document.querySelector("audio")
+// var audio = document.querySelector("audio")
+// audio.play();
